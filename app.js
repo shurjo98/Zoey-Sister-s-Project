4855abad95fcbed626673810a9ee7a58335b6dbb
shurjo98 <mehrabfazle97@icloud.com>
2021-03-10 19:39:14 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(express.static('public'))
+
+
+
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});
