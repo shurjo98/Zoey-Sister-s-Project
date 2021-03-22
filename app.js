445cdb40135951bcec723f42f562a8a9047c9a64
shurjo98 <mehrabfazle97@icloud.com>
2021-03-22 18:39:24 +0800

    Fixing cards

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 
 
 app.get("/", function(req,res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 
